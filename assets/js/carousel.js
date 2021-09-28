172a12b54dec022bc447b0dc96672f2969ba2c30
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-09-28 17:17:09 +0800

    improved animation performance of carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,48 +2,66 @@ const carouselItems = document.querySelectorAll('.carousel-item');
 const prevBtn = document.querySelector('.btn-prev');
 const nextBtn = document.querySelector('.btn-next');
 
-//images are in reversed order
+//images are in reversed order so the first image is the last in the NodeList
 let currIndex = carouselItems.length - 1;
 let prevIndex = 0;
 
-function showNextImage() {
-    setTimeout(() => {
-        carouselItems[currIndex].classList.remove('notransition');
-        carouselItems[currIndex].classList.remove('from-left');
-    }, 10);
-}
+let isAnimating = false;
 
-function showPrevImage() {
-    setTimeout(() => {
-        carouselItems[currIndex].classList.remove('notransition');
-        carouselItems[currIndex].classList.remove('from-right');
-    }, 10);
-}
+carouselItems.forEach((item) => (item.ontransitionend = () => (isAnimating = false)));
 
 prevBtn.addEventListener('click', () => {
+    //don't animate if an image is already being animated
+    //to prevent multiple animation renders
+    if (isAnimating) return;
+
     prevIndex = currIndex;
     currIndex = currIndex - 1 < 0 ? carouselItems.length - 1 : currIndex - 1;
-    carouselItems[currIndex].classList.add('notransition');
+    isAnimating = true;
 
-    setTimeout(() => {
-        carouselItems[currIndex].classList.remove('from-left');
-        carouselItems[currIndex].classList.add('from-right');
-        carouselItems[prevIndex].classList.add('from-left');
-
-        showPrevImage();
-    }, 10);
+    showPrevImage();
 });
 
 nextBtn.addEventListener('click', () => {
+    //don't animate if an image is already being animated
+    //to prevent multiple animation renders
+    if (isAnimating) return;
+
     prevIndex = currIndex;
     currIndex = currIndex + 1 > carouselItems.length - 1 ? 0 : currIndex + 1;
-    carouselItems[currIndex].classList.add('notransition');
+    isAnimating = true;
 
-    setTimeout(() => {
-        carouselItems[currIndex].classList.remove('from-right');
-        carouselItems[currIndex].classList.add('from-left');
-        carouselItems[prevIndex].classList.add('from-right');
-
-        showNextImage();
-    }, 10);
+    showNextImage();
 });
+
+function showPrevImage() {
+    //set initial image position by adding x-offset
+    carouselItems[currIndex].classList.add('from-right');
+    //this should not be animated
+    carouselItems[currIndex].classList.add('notransition');
+    //offset from previous animation should be removed
+    carouselItems[currIndex].classList.remove('from-left');
+
+    //animate to the proper position by removing x-offset
+    setTimeout(() => {
+        carouselItems[currIndex].classList.remove('notransition');
+        carouselItems[prevIndex].classList.add('from-left');
+        carouselItems[currIndex].classList.remove('from-right');
+    }, 0);
+}
+
+function showNextImage() {
+    //set initial image position by adding x-offset
+    carouselItems[currIndex].classList.add('from-left');
+    //this should not be animated
+    carouselItems[currIndex].classList.add('notransition');
+    //offset from previous animation should be removed
+    carouselItems[currIndex].classList.remove('from-right');
+
+    //animate to the proper position by removing x-offset
+    setTimeout(() => {
+        carouselItems[currIndex].classList.remove('notransition');
+        carouselItems[prevIndex].classList.add('from-right');
+        carouselItems[currIndex].classList.remove('from-left');
+    }, 0);
+}
