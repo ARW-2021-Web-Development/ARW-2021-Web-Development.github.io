2b554c632232e6b197dab945f8eb80276901404f
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-09-28 16:53:29 +0800

    added animation to image carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,23 +2,48 @@ const carouselItems = document.querySelectorAll('.carousel-item');
 const prevBtn = document.querySelector('.btn-prev');
 const nextBtn = document.querySelector('.btn-next');
 
-let currIndex = 0;
-showCurrentImage();
+//images are in reversed order
+let currIndex = carouselItems.length - 1;
+let prevIndex = 0;
 
-function showCurrentImage() {
-    carouselItems.forEach((item, index) => {
-        let visibility;
-        index === currIndex ? (visibility = 'visible') : (visibility = 'hidden');
-        item.style.visibility = visibility;
-    });
+function showNextImage() {
+    setTimeout(() => {
+        carouselItems[currIndex].classList.remove('notransition');
+        carouselItems[currIndex].classList.remove('from-left');
+    }, 10);
+}
+
+function showPrevImage() {
+    setTimeout(() => {
+        carouselItems[currIndex].classList.remove('notransition');
+        carouselItems[currIndex].classList.remove('from-right');
+    }, 10);
 }
 
 prevBtn.addEventListener('click', () => {
+    prevIndex = currIndex;
     currIndex = currIndex - 1 < 0 ? carouselItems.length - 1 : currIndex - 1;
-    showCurrentImage();
+    carouselItems[currIndex].classList.add('notransition');
+
+    setTimeout(() => {
+        carouselItems[currIndex].classList.remove('from-left');
+        carouselItems[currIndex].classList.add('from-right');
+        carouselItems[prevIndex].classList.add('from-left');
+
+        showPrevImage();
+    }, 10);
 });
 
 nextBtn.addEventListener('click', () => {
+    prevIndex = currIndex;
     currIndex = currIndex + 1 > carouselItems.length - 1 ? 0 : currIndex + 1;
-    showCurrentImage();
+    carouselItems[currIndex].classList.add('notransition');
+
+    setTimeout(() => {
+        carouselItems[currIndex].classList.remove('from-right');
+        carouselItems[currIndex].classList.add('from-left');
+        carouselItems[prevIndex].classList.add('from-right');
+
+        showNextImage();
+    }, 10);
 });
