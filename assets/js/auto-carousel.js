562c10713f018226e7486b99056096daae09deb7
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-11-09 21:35:13 +0800

    improved autocarousel, fixed social btns in home page

--- assets/js/auto-carousel.js
+++ assets/js/auto-carousel.js
@@ -3,30 +3,17 @@ const carouselItems = document.querySelectorAll('.carousel-item');
 //images are in reversed order so the first image is the last in the
 let currIndex = 0;
 let prevIndex = 0;
-
+let timer = createTimer();
 let isAnimating = false;
 
-let timer = setInterval(() => {
-    if (isAnimating) return;
-
-    prevIndex = currIndex;
-    currIndex = currIndex + 1 > carouselItems.length - 1 ? 0 : currIndex + 1;
-
-    showPrevImage();
-}, 5000);
-
-carouselItems.forEach((item) => (item.ontransitionend = () => (isAnimating = false)));
 carouselItems.forEach((item) => {
+    item.ontransitionend = () => {
+        isAnimating = false;
+    };
     item.onmouseenter = () => clearInterval(timer);
+
     item.onmouseleave = () => {
-        timer = setInterval(() => {
-            if (isAnimating) return;
-
-            prevIndex = currIndex;
-            currIndex = currIndex + 1 > carouselItems.length - 1 ? 0 : currIndex + 1;
-
-            showPrevImage();
-        }, 5000);
+        timer = createTimer();
     };
 });
 
@@ -38,26 +25,25 @@ function showPrevImage() {
     //offset from previous animation should be removed
     carouselItems[currIndex].classList.remove('from-left');
 
+    isAnimating = true;
     //animate to the proper position by removing x-offset
     setTimeout(() => {
         carouselItems[currIndex].classList.remove('notransition');
         carouselItems[prevIndex].classList.add('from-left');
         carouselItems[currIndex].classList.remove('from-right');
-    }, 0);
+    }, 100);
 }
 
-function showNextImage() {
-    //set initial image position by adding x-offset
-    carouselItems[currIndex].classList.add('from-left');
-    //this should not be animated
-    carouselItems[currIndex].classList.add('notransition');
-    //offset from previous animation should be removed
-    carouselItems[currIndex].classList.remove('from-right');
+function createTimer() {
+    return setInterval(() => {
+        if (isAnimating) {
+            clearTimeout(timer);
+            timer = createTimer();
+            return;
+        }
+        prevIndex = currIndex;
+        currIndex = currIndex + 1 > carouselItems.length - 1 ? 0 : currIndex + 1;
 
-    //animate to the proper position by removing x-offset
-    setTimeout(() => {
-        carouselItems[currIndex].classList.remove('notransition');
-        carouselItems[prevIndex].classList.add('from-right');
-        carouselItems[currIndex].classList.remove('from-left');
-    }, 0);
+        showPrevImage();
+    }, 5000);
 }
