517438061d8ad5f5a163ff1d191c700e63d0af26
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-11-21 03:54:59 +0800

    added loading to only the index page

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -2,5 +2,7 @@ const loadingScreen = document.querySelector('.loading-screen');
 const loadingIcon = document.querySelector('#loading-icon');
 
 window.addEventListener('load', () => {
-    loadingScreen.classList.add('hide');
+    setTimeout(() => {
+        loadingScreen.classList.add('hide');
+    }, 5500);
 });
