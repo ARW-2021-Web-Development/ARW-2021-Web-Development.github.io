1d955c6035105dba9d59f1bdb535c77fdefb211b
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-11-21 04:17:00 +0800

    Update loading.js

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -4,5 +4,5 @@ const loadingIcon = document.querySelector('#loading-icon');
 window.addEventListener('load', () => {
     setTimeout(() => {
         loadingScreen.classList.add('hide');
-    }, 5500);
+    }, 2500);
 });
