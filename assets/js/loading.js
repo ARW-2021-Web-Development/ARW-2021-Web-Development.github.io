04bca41305d9d86826802eed40713bdd523c94aa
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-11-21 02:46:59 +0800

    updated loading page

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,17 +1,6 @@
 const loadingScreen = document.querySelector('.loading-screen');
 const loadingIcon = document.querySelector('#loading-icon');
 
-let removeLoadingPage = false;
-
 window.addEventListener('load', () => {
-    removeLoadingPage = true;
-});
-
-loadingIcon.addEventListener('animationiteration', () => {
-    if (removeLoadingPage) {
-        loadingIcon.classList.remove('rotate-logo');
-        setTimeout(() => {
-            loadingScreen.classList.add('hide');
-        }, 1500);
-    }
+    loadingScreen.classList.add('hide');
 });
